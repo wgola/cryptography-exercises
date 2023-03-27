20ceb1f39278717e510bd792dbf2324a5d7a50f3
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-03-27 20:00:22 +0200

    fixed minor issues

--- lab02/prepare.py
+++ lab02/prepare.py
@@ -1,7 +1,7 @@
 def prepare():
     try:
         with open("orig.txt", "r") as orig_file:
-            orig_txt = orig_file.read().replace("\n", " ")
+            orig_txt = orig_file.read().replace("\n", " ").lower()
 
         with open("plain.txt", "w+") as plain_file:
             new_line = ""
@@ -12,8 +12,10 @@ def prepare():
                     new_line = ""
 
             if len(new_line) != 0:
-                new_line += (64 - len(new_line))*"x"
+                new_line += " " + (63 - len(new_line))*"x"
                 plain_file.write(new_line)
+
+        print("Text prepared succesfully!")
 
     except:
         print("File 'orig.txt' not found!")
