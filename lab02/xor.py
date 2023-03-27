70a47db8d3ae8986c8c07b1c99de63d2766f017e
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-03-27 15:27:09 +0200

    initialized exercise

---
+++ lab02/xor.py
@@ -0,0 +1,28 @@
+import sys
+
+# Autor: Wojciech Gola
+
+
+def main():
+    possible_options = ["-p", "-e", "-k"]
+
+    if len(sys.argv) != 2:
+        print("Wrong number of arguments!")
+        return
+
+    option = sys.argv[1]
+
+    if option not in possible_options:
+        print("Wrong argument!")
+        return
+
+    if option == "-p":
+        print("prepare")
+    elif option == "-e":
+        print("encrypt")
+    else:
+        print("cryptoanalysis")
+
+
+if __name__ == "__main__":
+    main()
